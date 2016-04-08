9612f3e3b2c2e08889585377a2dd0997e63a66cc
MTCDetroit <mtcdetroit@hotmail.com>
2016-04-07 20:51:01 -0400

    Delete MakeGit.cmd

--- MakeGit.cmd
+++
@@ -1,6 +0,0 @@
-echo "# BrainPad-Lab" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/stacyh3/BrainPad-Lab.git
-git push -u origin master
